d75c990ba657944a302a813f17a2eb06264e8d31
HenriqueRL55 <henriquerl22@outlook.com>
2022-10-02 20:51:22 -0300

    feat: created sign-in page

---
+++ lumni/src/pages/sign-in/sign-in.component.jsx
@@ -0,0 +1,10 @@
+/* Components */
+import SignIn from "../../components/Sign-in/sign-in.component";
+
+export function SignInPage() {
+    return (
+        <>
+            <SignIn />
+        </>
+    );
+}
